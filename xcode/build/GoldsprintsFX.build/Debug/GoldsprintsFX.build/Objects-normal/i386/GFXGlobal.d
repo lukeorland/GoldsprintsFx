5ad76c07b1e5ddde13e7a3f530f69073ae9a4939
Luke Orland <orluke@gmail.com>
2014-10-09 23:08:42 -0400

    Replace xcode/build/ with .gitignore

--- xcode/build/GoldsprintsFX.build/Debug/GoldsprintsFX.build/Objects-normal/i386/GFXGlobal.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/charlie/Documents/Development/_libs/cpp/Cinder_086/apps/sharkbox/GoldsprintsFX/src/GFXGlobal.cpp \
-  /Users/charlie/Documents/Development/_libs/cpp/Cinder_086/apps/sharkbox/GoldsprintsFX/src/GFXGlobal.h \
-  ../../../../include/cinder/app/AppNative.h
